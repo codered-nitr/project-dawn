97a31780da427eda344d7f311069f4209b202864
Nishith G. Behera <zenolusx@gmail.com>
2020-01-03 21:50:36 +0530

    Updates

---
+++ precache-manifest.cf06f9e171508efd95f99fae43ad4a4e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc2553f2ec63f0f6d68018ee65a2384",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce45b08edc2f38786ce9",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "49c020eeb1672a950a61",
+    "url": "/static/css/main.051b7511.chunk.css"
+  },
+  {
+    "revision": "ce45b08edc2f38786ce9",
+    "url": "/static/js/2.75287243.chunk.js"
+  },
+  {
+    "revision": "a003614a676dfd62d5c2a8a236393cff",
+    "url": "/static/js/2.75287243.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49c020eeb1672a950a61",
+    "url": "/static/js/main.3dfe68ca.chunk.js"
+  },
+  {
+    "revision": "2b7dd6ec8ba8dfa3241d",
+    "url": "/static/js/runtime-main.089359c8.js"
+  },
+  {
+    "revision": "150802d1a87f3fee783feaca123b6926",
+    "url": "/static/media/CodeRed Inductions.150802d1.png"
+  },
+  {
+    "revision": "abcb073d95201a4eb3f2aea41ed0453c",
+    "url": "/static/media/Enigma Coming soon.abcb073d.png"
+  },
+  {
+    "revision": "687b022394fe445e055a98f349c074d5",
+    "url": "/static/media/EnigmaTopics.687b0223.png"
+  },
+  {
+    "revision": "73a836518e6f26de625a1ca7b1df71af",
+    "url": "/static/media/code-red.73a83651.png"
+  }
+]);
